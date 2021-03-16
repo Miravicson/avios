f1a3f991661a72e2c3f0fd3d09c059d0030021ce
Victor Ughonu <victorughonu95@gmail.com>
2021-03-16 08:08:26 +0100

    some frontend work

--- server/controllers/viewController.js
+++ server/controllers/viewController.js
@@ -1,7 +1,26 @@
+import sqlDB from '@models/index';
+import { catchAsync } from '@utils';
+
+const Products = sqlDB.products;
+
 export const index = (req, res) => {
   res
     .status(200)
     .render('index', { title: 'Avios', message: 'Up and running' });
+};
+
+export const allProducts = catchAsync(async (req, res, next) => {
+  const products = await Products.findAll({});
+  res.status(200).render('allProducts', {
+    title: 'All Products',
+    products: products,
+  });
+});
+export const product = (req, res) => {
+  res.status(200).render('product', {});
+};
+export const addProduct = (req, res) => {
+  res.status(200).render('addProduct', { title: 'Add Product' });
 };
 
 export const nonIndex = (req, res) => {
--- server/routes/v1/viewRoutes.js
+++ server/routes/v1/viewRoutes.js
@@ -4,4 +4,7 @@ const viewController = require('@controllers/viewController');
 const router = Router();
 
 router.get('/', viewController.index);
+router.get('/products', viewController.allProducts);
+router.get('/add-product', viewController.addProduct);
+router.get('/products/:product_id', viewController.product);
 export default router;
